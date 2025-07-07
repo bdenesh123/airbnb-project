bc087004c4140e3de920a5fa0c8256dd7a57aece
bdenesh123 <drajan@wvb.com>
2025-07-07 19:43:18 +0800

    spinner fix

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -15,8 +15,8 @@ const Route = ({ children, isProtected }) => {
   }, [isProtected, navigate, token]);
 
   return token === undefined || user === undefined ? (
-    <div className='absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center'>
-      <Spinner />
+    <div className=' fixed inset-0 z-50 flex items-center justify-center'>
+      <Spinner className='h-10 w-10' />
     </div>
   ) : (
     children
